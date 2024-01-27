0f5371d3eab89005935e0bcce7e99ac83fb2647a
jimdeekepler <49359725+jimdeekepler@users.noreply.github.com>
2024-01-27 12:18:40 +0100

    Using utils in linescroller.py

--- scrolltext/linescroller.py
+++ scrolltext/linescroller.py
@@ -1,30 +1,27 @@
 """
 A simple side scrolling text application.
 """
+from utils import CharacterScroller
 from os import getenv
 from time import sleep
-import shutil
 
 
 DEF_SCROLL_TEXT = """\
 Hello, this is a  classic side scrolling text. You can override it by setting the \
 environment variable 'SCROLL_TEXT'. It is supposed to be a simple example."""
 SCROLL_TEXT = getenv("SCROLL_TEXT") or DEF_SCROLL_TEXT
-VISIBILE_TEXT_LENGTH = shutil.get_terminal_size((80, 1)).columns
+VISIBILE_TEXT_LENGTH = 80
 
 
 def linescroller():
     """
     Prints a text in a side-scrolling manner.
     """
-    blanks = " " * VISIBILE_TEXT_LENGTH
-    complete_text = blanks + SCROLL_TEXT + blanks
-    for start in range(len(complete_text)):
-        end = start + VISIBILE_TEXT_LENGTH
-        win_text = complete_text[start:end]
+    scroller = CharacterScroller(VISIBILE_TEXT_LENGTH, VISIBILE_TEXT_LENGTH, SCROLL_TEXT)
+    for text in scroller:
+        win_text = text
         print(win_text, end="\r")
         sleep(.15)
-        start += 1
 
 
 if __name__ == "__main__":
