ffceded3b558426d1e1204b7c6b6322c36421bdd
jimdeekepler <49359725+jimdeekepler@users.noreply.github.com>
2024-01-27 13:37:31 +0100

    Cosmetics in utils.py addressed.

--- scrolltext/utils.py
+++ scrolltext/utils.py
@@ -1,6 +1,8 @@
 """
 Utilities for line-based text scrollers.
 """
+
+
 class CharacterScroller:
     """
     Utility class  for all character based text-scrollers.
@@ -8,11 +10,11 @@ class CharacterScroller:
 
     def __init__(self, *args):
         """Objects init method.
-        :param args[0]: The number of characters of visible text 
+        :param args[0]: The number of characters of visible text
         :type args[0]: int
         :param args[1]: The number of leading and trailing blank characters to add
         :type args[1]: int
-        :param args[2]: The text to scroll 
+        :param args[2]: The text to scroll
         :type args[2]: str
         """
         self.visible_text_length = int(args[0])
@@ -28,7 +30,7 @@ class CharacterScroller:
         """
         Gives the next visible text to display by the client-program.
 
-        :returns: A str object of visible text length 
+        :returns: A str object of visible text length
         :rtype: str
         """
         if self.pos == self.terminal_pos:
