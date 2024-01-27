7e048c7b3125facb678168831d36062582e5dfff
jimdeekepler <49359725+jimdeekepler@users.noreply.github.com>
2024-01-27 12:55:04 +0100

    Added some doc strings.

--- scrolltext/utils.py
+++ scrolltext/utils.py
@@ -3,17 +3,17 @@ Utilities for line-based text scrollers.
 """
 class CharacterScroller:
     """
-    Utility class  for all character based text-scroller applications.
+    Utility class  for all character based text-scrollers.
     """
 
-    def __init__(self, *args, **argv):
-        """
-        ob das klappt???
-        
-        args[0] : int -> number of characters of visible text 
-        args[1] : int -> number of leading and trailing blank characters to add
-        args[2] : str -> the text to scroll 
-
+    def __init__(self, *args):
+        """Objects init method.
+        :param args[0]: The number of characters of visible text 
+        :type args[0]: int
+        :param args[1]: The number of leading and trailing blank characters to add
+        :type args[1]: int
+        :param args[2]: The text to scroll 
+        :type args[2]: str
         """
         self.visible_text_length = int(args[0])
         self.blanks = int(args[1]) * " "
@@ -22,9 +22,15 @@ class CharacterScroller:
         self.terminal_pos = len(self.complete_text)
 
     def __iter__(self):
-        return iter(self._next, None)
+        return iter(self.next, None)
 
-    def _next(self):
+    def next(self):
+        """
+        Gives the next visible text to display by the client-program.
+
+        :returns: A str object of visible text length 
+        :rtype: str
+        """
         if self.pos == self.terminal_pos:
             return None
         end = self.pos + self.visible_text_length
