95fb617bb6a4e06d8e6cf80a6747e9a8b45a9e7d
Jim Dee Kepler <jimdee@gmail.com>
2024-02-04 08:45:38 +0100

    Removed unused imports from init.

--- scrolltext/__init__.py
+++ scrolltext/__init__.py
@@ -1,5 +1,4 @@
 """A little toy for scrolling text. (TODO: duplicate with setup.cfg)
 """
-from .cursestext import *
-from .linescroller import *
-from .utils import *
+from .cursestext import main
+from .linescroller import linescroller
