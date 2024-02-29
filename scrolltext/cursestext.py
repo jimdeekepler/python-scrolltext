7dcf035685d58a3419c3bbda9902ea71eee053e9
Jim Dee Kepler <jimdee@gmail.com>
2024-03-01 00:45:56 +0100

    Added env-var 'SCROLL_BOX'.

--- scrolltext/cursestext.py
+++ scrolltext/cursestext.py
@@ -9,6 +9,11 @@ from .utils import (CharacterScroller,
                     get_linenum, SCROLL_TEXT)
 
 TRUE_CHARACTERS = ["1", "y", "yes"]
+BOX = getenv("SCROLL_BOX")
+if BOX is not None:
+    BOX = BOX in TRUE_CHARACTERS
+else:
+    BOX = True
 VERBOSE = getenv("VERBOSE") in TRUE_CHARACTERS or False
 if VERBOSE:
     logging.basicConfig(filename="cursesscroller.log", filemode="w", level=logging.DEBUG)
@@ -27,40 +32,39 @@ def curses_scroller(win):
     :param win: Internal curses based object
     :type win: curses._window
     """
-    box = True
-    if box:
+    if BOX:
         win.box()
     curses.curs_set(0)  # Hide the cursor
     winsize = win.getmaxyx()
     visibile_height = winsize[0] - 1
-    visibile_text_length = winsize[1] - (2 if box else 0)
+    visibile_text_length = winsize[1] - (2 if BOX else 0)
     log.debug("win dimensions: (%d, %d)", visibile_text_length, visibile_height)
-    line = get_linenum(3, visibile_height - (1 if box else 0))
+    line = get_linenum(3, visibile_height - (1 if BOX else 0))
     log.debug("screenline %d", line)
     scroller = CharacterScroller(visibile_text_length,
                                  visibile_text_length, SCROLL_TEXT)
     win.addstr(1, 10, "Scroll-Text")
-    add_quit_text(win, box, line, visibile_height)
+    add_quit_text(win, line, visibile_height)
     win.timeout(125)
     for text in scroller:
         win_text = text
-        if not box and line == visibile_height:
+        if not BOX and line == visibile_height:
             win_text = text[:-1]
-        win.addstr(line, (1 if box else 0), win_text)
+        win.addstr(line, (1 if BOX else 0), win_text)
         win.redrawwin()
         if check_quit(win):
             return
         win.redrawwin()
 
 
-def add_quit_text(win, box, line, visibile_height):
+def add_quit_text(win, line, visibile_height):
     """
     Adds a hint message to win.
     """
-    if not box and line == visibile_height:
+    if not BOX and line == visibile_height:
         win.addstr(visibile_height - 2, 0, " You can quit with 'q' or 'Q'.")
     else:
-        win.addstr(visibile_height, (2 if box else 0), " You can quit with 'q' or 'Q'.")
+        win.addstr(visibile_height, (2 if BOX else 0), " You can quit with 'q' or 'Q'.")
 
 
 def check_quit(win):
