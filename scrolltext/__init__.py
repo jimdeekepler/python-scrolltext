d3e11482216cb96af257a8ff4fb9aed131125cb2
Jim Dee Kepler <jimdee@gmail.com>
2024-03-12 10:48:05 +0100

    Renamed cursestext 'main' to 'work'

--- scrolltext/__init__.py
+++ scrolltext/__init__.py
@@ -1,5 +1,5 @@
 """
 Python Module scrolltext
 """
-from .cursestext import main as cursesscroller  # noqa: F401
+from .cursestext import work as cursesscroller  # noqa: F401
 from .linescroller import linescroller          # noqa: F401
