f7eb9283aef8bdba843a8fc0fbe1a72707883834
Jim Dee Kepler <jimdee@gmail.com>
2024-03-12 10:47:21 +0100

    Using new utils interface in module cli.

--- scrolltext/cli.py
+++ scrolltext/cli.py
@@ -4,18 +4,41 @@ Main entry point for scrolltext
 import sys
 from scrolltext import cursesscroller
 from scrolltext import linescroller
+from scrolltext.utils import init_utils
 
 
 def main():
     """
     Main method.
     """
-    invoke = linescroller
+    action = _parse_args()
+    try:
+        cfg = init_utils(False)
+        action = action or _str_to_action_type(cfg["main"]["action"])
+        action(False)
+    except NameError as e:
+        print("NameError occured: " + str(e))
+        print("Probalby check config?")
+
+
+def _parse_args():  # pylint: disable=inconsistent-return-statements  (R1710)
+    action = None
     for arg in sys.argv[1:]:
         if "cursestext" == arg:
-            invoke = cursesscroller
+            action = cursesscroller
+        elif "linescroller" == arg:
+            action = linescroller
+    return action
 
-    invoke()
+
+def _str_to_action_type(action):
+    if "cursestext" == action:
+        action = cursesscroller
+    elif "linescroller" == action:
+        action = linescroller
+    else:
+        raise RuntimeError("Unknown 'action' type")
+    return action
 
 
 if __name__ == "__main__":
