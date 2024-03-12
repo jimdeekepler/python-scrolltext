1d82283be2d6f18fb0cc0fa3defeaa1ef2769de7
Jim Dee Kepler <jimdee@gmail.com>
2024-03-12 10:27:14 +0100

    Using new utils interface

--- scrolltext/linescroller.py
+++ scrolltext/linescroller.py
@@ -3,26 +3,26 @@ A simple side scrolling text application.
 """
 import shutil
 from time import sleep
-from .utils import (CLEAR, HOME, IS_WINDOWS, UP_ONE_ROW, CharacterScroller, get_linenum,
-                    SCROLL_TEXT, scroll_direction, scrollspeedsec)
-
+from .utils import CLEAR, HOME, IS_WINDOWS, UP_ONE_ROW, CharacterScroller, init_utils
 
 if not IS_WINDOWS:
-    from .getchtimeout import GetchWithTimeout
+    from scrolltext.getchtimeout import GetchWithTimeout
 
 
 VISIBILE_TEXT_LENGTH = shutil.get_terminal_size()[0]
 
 
-def linescroller():
+def linescroller(write_config):
     """
     Main entry point for linescroller.
+    :param write_config: Write initial config
+    :type: bool
     """
     getch = None
     if not IS_WINDOWS:
         getch = GetchWithTimeout()
     try:
-        _linescroller(getch)
+        _linescroller(getch, write_config)
     except RuntimeError:
         pass
     finally:
@@ -30,16 +30,20 @@ def linescroller():
             getch.cleanup()
 
 
-def _linescroller(getch):
+def _linescroller(getch, write_config):
     """
     Prints a text in a side-scrolling manner.
     """
+    cfg = init_utils(write_config)
+    argv = {}
+    argv["term_rows"] = shutil.get_terminal_size()[1]
+    argv["term_columns"] = shutil.get_terminal_size()[0]
+    argv["min_scroll_line"] = 0
+    scroller = CharacterScroller(cfg, **argv)
+
     print(f"{CLEAR}{HOME}", end="")
-    line = get_linenum(0,  shutil.get_terminal_size()[1])
-    if line > 0:
-        _move_to_line(line)
-    scroller = CharacterScroller(VISIBILE_TEXT_LENGTH, VISIBILE_TEXT_LENGTH,
-                                 SCROLL_TEXT, scroll_direction, scrollspeedsec)
+    if scroller.line > 0:
+        _move_to_line(scroller.line)
     for text in scroller:
         win_text = text
         print(win_text, end="\r")
